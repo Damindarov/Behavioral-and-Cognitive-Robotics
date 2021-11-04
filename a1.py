ada17ab0a989ea244875a3c8558b77a7bf0b4cf8
Damindarov <damin4@mail.ru>
2021-11-04 18:09:53 +0300

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,22 @@
+import gym
+import time
+
+env = gym.make('CartPole-v0')
+observation = env.reset()
+# print(observation)
+done = False
+fitness = 0
+step_counter = 0
+while not done:
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
+    fitness += reward
+    print(f"\nStep: {step_counter}\nObservation vector: {observation}\n"
+        f"Action vector: {action}\nReward: {reward}\nFitness: {fitness}")
+    if done:
+        time.sleep(3)
+    env.render()
+    time.sleep(0.1)
+    step_counter += 1
+env.close()
+print(fitness)
